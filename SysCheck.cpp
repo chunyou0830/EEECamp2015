27b4cbd8fca7a5bd9d8c6e47721370e268a4983a
Chunyou Yang <chunyou0830@gmail.com>
2015-05-26 22:17:31 +0800

    SysCheck_Test

--- SysCheck.cpp
+++ SysCheck.cpp
@@ -7,10 +7,9 @@ using namespace std;
 
 int main ()
 {
-   char command[50];
-
-   strcpy( command, "dir /B" );
-   cout << system(command);
-
-   return(0);
+char command[1000];
+strcpy( command, "dir /B /S D1.cpp D2.cpp D3.cpp D4.cpp D5.cpp D6.cpp D7.cpp D8.cpp D9.cpp GD1.cpp GD2.cpp GD3.cpp GD4.cpp GD5.cpp" );
+cout << system(command);
+system("exit");
+return(0);
 } 
